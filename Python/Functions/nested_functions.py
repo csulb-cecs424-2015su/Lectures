3a65d568a150e980e67e9f86403e5f64959bb4e0
nealterrell <neal.terrell@csulb.edu>
2015-07-20 16:21:17 -0700

    Fix example.

--- Python/Functions/nested_functions.py
+++ Python/Functions/nested_functions.py
@@ -8,16 +8,15 @@ def fun1(param1):
         local2.append(param2)
         return param2 % local1
 
-    ret = fun2
-
     # When a nested function is executed, its referencing environment
     # is created with references assigned to all values in the scope of the
     # outer function. When we call fun2, it will have bindings that reference
     # the values of local1, local2, and param1. If those are mutable values,
     # changes made in fun2 to their values will also affect fun1's scope.
-    local3 = fun2(99)
-    print local2
     
-    return ret
+    local3 = fun2(99)
+    return local3
 
 print fun1(13)
+
+# Why would you want to do this?
