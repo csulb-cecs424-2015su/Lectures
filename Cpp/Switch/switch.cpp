c3a3234b630ef130187b734cec3768803761a1c6
nealterrell <neal.terrell@csulb.edu>
2015-10-04 17:06:43 -0700

    Initial commit.

---
+++ Cpp/Switch/switch.cpp
@@ -0,0 +1,35 @@
+int main() {
+   int x = 5;
+
+   // The mighty switch statement.
+   switch (x) {
+   case 1:
+
+      break;
+   case 2:
+
+      break;
+   case 3:
+
+      break;
+
+   case 4:
+
+      break;
+   default:
+
+      break;
+   }
+
+   // How is this different than if/else?
+   if (x == 1) {
+   }
+   else if (x == 2) {
+   }
+   else if (x == 3) {
+   }
+   else if (x == 4) {
+   }
+   else {
+   }
+}
