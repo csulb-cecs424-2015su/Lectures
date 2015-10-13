c6f634a4b37a9642af596202fc7dd11d785702d5
nealterrell <neal.terrell@csulb.edu>
2015-10-13 16:10:20 -0700

    Redo example with list instead of vectors.

--- Clojure/quadratic-formula/src/quadratic_formula/core.clj
+++ Clojure/quadratic-formula/src/quadratic_formula/core.clj
@@ -5,16 +5,14 @@
   [a b c]
   (let [descr (- (* b b) (* 4 a c))]
     (cond
-      (< descr 0) []
-      (= descr 0) [(/ (- b) (* 2 a))]
+      (< descr 0) (list)
+      (= descr 0) (list (/ (- b) (* 2 a)))
       :else (let [sq (Math/sqrt descr)]
-              [(/ (+ (- b) sq) (* 2 a))
-               (/ (- (- b) sq) (* 2 a))]))))
+              (list (/ (+ (- b) sq) (* 2 a))
+                    (/ (- (- b) sq) (* 2 a)))))))
 
 (defn -main
   [& args]
-  (println (quadratic-formula 3 -13 -10))
-
   (let [ans (quadratic-formula 1 0 1)]
     (println (count ans) "solutions"
              (if (pos? (count ans))
