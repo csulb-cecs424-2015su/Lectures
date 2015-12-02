6171501451d65c57f7c6efe922863bfc775da545
nealterrell <neal.terrell@csulb.edu>
2015-12-01 19:52:40 -0800

    Slight tweaks.

--- Erlang/Fibonacci/src/fibonacci.erl
+++ Erlang/Fibonacci/src/fibonacci.erl
@@ -1,5 +1,5 @@
 -module(fibonacci).
--export([fib/1, fib3serial/1, fibloop/0, fib3parallel/1]).
+-export([fib/1, fib3serial/1, fib3parallel/1, fibThread/2]).
 
 fib(0) -> 1;
 fib(1) -> 1;
@@ -9,31 +9,31 @@ fib(_) -> undefined.
 %% Three fib numbers in serial, as three values in a list.
 fib3serial(N) -> [fib(N), fib(N), fib(N)].
 
-%% A process to receive a message requesting a fibonacci number,
-%% calculate that number, then send it back to the requesting process.
-fibloop() ->
-  receive
-    {From, N} -> From ! {self(), fib(N)}
-  end.
+
+
+
+%% A process to calculate a Fibonacci number and send it back
+%% to a waiting process process.
+fibThread(SendTo, N) ->
+  Answer = fib(N),
+  SendTo ! {self(), Answer}.
 
 %% A process to calculate 3 fib numbers by spawning 3 threads, sending
 %% each a message, then handling their responses to build a list of
 %% 3 values.
 fib3parallel(N) ->
-  P1 = spawn(fun fibloop/0),
-  P2 = spawn(fun fibloop/0),
-  P3 = spawn(fun fibloop/0),
-  %% Dispatch the messages to start the calculations.
-  P1 ! {self(), N},
-  P2 ! {self(), N},
-  P3 ! {self(), N},
-  %% Await the replies; 3 expected
+  P1 = spawn(?MODULE, fibThread, [self(), N]),
+  P2 = spawn(?MODULE, fibThread, [self(), N]),
+  P3 = spawn(?MODULE, fibThread, [self(), N]),
+  %% Await the replies; 3 expected. Return them.
   awaitfibs(3, []).
 
-awaitfibs(ThreadCount, Results) ->
+%% A loop to receive a certain number of messages contining fib
+%% results, then returna list containing all the results.
+awaitfibs(ThreadsLeft, Results) ->
   receive
-    {From, FibN} when length(Results) =:= ThreadCount-1 ->
+    {From, FibN} when ThreadsLeft =:= 1 ->
       [FibN|Results];
     {From, FibN} ->
-      awaitfibs(ThreadCount, [FibN|Results])
+      awaitfibs(ThreadsLeft - 1, [FibN|Results])
   end.
